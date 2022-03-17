e082ddd6a5ad699800c8cf228064b791da00943d
michael <software.engr.full.stack@gmail.com>
2022-03-16 17:01:22 -0700

    Clean up front end

--- client/reactjs/src/App.js
+++ client/reactjs/src/App.js
@@ -2,9 +2,6 @@ import React, { useState, useEffect } from 'react';
 
 import fetchData from './data';
 
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   const [data, setData] = useState();
 
@@ -17,18 +14,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        Test
       </header>
     </div>
   );
