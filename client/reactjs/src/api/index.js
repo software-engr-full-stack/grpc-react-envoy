4c4d9050bf80cde5f78478b5606bbf750a1d997e
michael <software.engr.full.stack@gmail.com>
2022-05-08 20:32:07 -0700

    Configure for deployment

--- client/reactjs/src/api/index.js
+++ client/reactjs/src/api/index.js
@@ -14,7 +14,7 @@ import {
 const retrieve = (zipCode) => {
   if (presentInSaved(zipCode)) return Promise.resolve(presentInSaved(zipCode));
 
-  const svc = new MainServicePromiseClient('/api');
+  const svc = new MainServicePromiseClient('');
   const request = new MainServiceRequest();
 
   request.setZipcode(zipCode);
