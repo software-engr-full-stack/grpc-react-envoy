53a12313de92bfd47c0574981c846b26a69f2eb4
software-engr-full-stack <software.engr.full.stack@gmail.com>
2023-02-21 02:07:13 -0800

    Add "humid" to weather table

--- server/weather.go
+++ server/weather.go
@@ -2,26 +2,27 @@ package main
 
 import (
     "context"
-    "time"
-    "net/http"
-    "io"
     "encoding/json"
     "fmt"
+    "io"
+    "net/http"
+    "time"
+
     "github.com/pkg/errors"
 )
 
 type WeatherDataType struct {
-    Product string `json:"product"`
-    Init uint `json:"init,string"`
+    Product    string           `json:"product"`
+    Init       uint             `json:"init,string"`
     DataSeries []DataSeriesType `json:"dataseries"`
 }
 
 type DataSeriesType struct {
-    Date float64 `json:"date"`
-    Temp2m Temp2mType `json:"temp2m"`
-    Wind10mMax float32 `json:"wind10_max"`
+    Date       float64    `json:"date"`
+    Temp2m     Temp2mType `json:"temp2m"`
+    Wind10mMax float32    `json:"wind10_max"`
 
-    Weather string `json:"weather"`
+    Weather     string `json:"weather"`
     WeatherName string `json:"weather_name"`
     WeatherDesc string `json:"weather_desc"`
 }
@@ -71,20 +72,21 @@ func weather(latitude, longitude float64) (WeatherDataType, error) {
     }
 
     weatherTable := map[string]map[string]string{
-        "clear": map[string]string{"desc": "Total cloud cover less than 20%%", "name": "Clear"},
+        "clear":   map[string]string{"desc": "Total cloud cover less than 20%%", "name": "Clear"},
         "pcloudy": map[string]string{"desc": "Total cloud cover between 20%%-60%%", "name": "Partly cloudy"},
         "mcloudy": map[string]string{"desc": "Total cloud cover between 20%%-80%%", "name": "Mostly cloudy"},
-        "cloudy": map[string]string{"desc": "Total cloud cover over over 80%%", "name": "Cloudy"},
+        "cloudy":  map[string]string{"desc": "Total cloud cover over over 80%%", "name": "Cloudy"},
         "lightrain": map[string]string{
             "desc": "Precipitation rate less than 4mm/hr with total cloud cover more than 80%%",
             "name": "Light rain",
         },
-        "rain": map[string]string{"desc": "Rain with total cloud cover over 80%%", "name": "Rain"},
+        "rain":      map[string]string{"desc": "Rain with total cloud cover over 80%%", "name": "Rain"},
         "lightsnow": map[string]string{"desc": "Precipitation rate less than 4mm/hr", "name": "Light snow"},
-        "snow": map[string]string{"desc": "Snow with total cloud cover over 80%%", "name": "Snow"},
-        "ts": map[string]string{"desc": "Lifted Index less than -5", "name": "Thunderstorm"},
-        "tsrain": map[string]string{"desc": "Lifted Index less than -5 with rain", "name": "Thunderstorm with rain"},
-        "ishower": map[string]string{"desc": "Precipitation rate less than 4mm/hr with total cloud cover less than 60%%", "name": "Shower"},
+        "snow":      map[string]string{"desc": "Snow with total cloud cover over 80%%", "name": "Snow"},
+        "ts":        map[string]string{"desc": "Lifted Index less than -5", "name": "Thunderstorm"},
+        "tsrain":    map[string]string{"desc": "Lifted Index less than -5 with rain", "name": "Thunderstorm with rain"},
+        "ishower":   map[string]string{"desc": "Precipitation rate less than 4mm/hr with total cloud cover less than 60%%", "name": "Shower"},
+        "humid":     map[string]string{"desc": "???", "name": "Humid"},
     }
 
     for ix, ds := range wdata.DataSeries {
@@ -101,4 +103,3 @@ func weather(latitude, longitude float64) (WeatherDataType, error) {
 
     return wdata, nil
 }
-
